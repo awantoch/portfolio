f8980bbacea1ba38c528d55a79b1ce9fb0c4180f
Alec M. Wantoch <alec@wantoch.com>
2025-05-06 15:10:56 -0700

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,16 @@ export const metadata: Metadata = {
     template: `%s | ${SITE_CONFIG.title}`,
   },
   description: SITE_CONFIG.description,
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
   openGraph: {
     title: SITE_CONFIG.title,
     description: SITE_CONFIG.description,
