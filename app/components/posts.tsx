c6c7dd49aef6cc378755c405c0a5d4e7fae1f914
Alec M. Wantoch <alec@wantoch.com>
2025-05-12 21:27:29 -0700

    styling

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -28,11 +28,11 @@ export function JournalEntries({ limit, showMore = false }: JournalEntriesProps)
             className="flex flex-col space-y-1 mb-4"
             href={`/journal/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-400 w-[100px] tabular-nums">
+            <div className="flex flex-row items-baseline gap-x-2">
+              <p className="text-neutral-400 min-w-[120px] whitespace-nowrap tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-neutral-100 tracking-tight">
+              <p className="text-neutral-100 tracking-tight font-semibold">
                 {post.metadata.title}
               </p>
             </div>
