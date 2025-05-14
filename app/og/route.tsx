bad78091a85fcd884bc80e2460dcba2608e56f44
Alec M. Wantoch <alec@wantoch.com>
2025-05-13 17:23:10 -0700

    style og image

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -16,24 +16,24 @@ export async function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-start justify-center" style={{ backgroundColor: '#18181b' }}>
-        <div tw="flex flex-col w-full h-full items-start justify-center p-16 pl-32">
-          <div tw="flex flex-col items-start justify-start w-full max-w-[900px]">
-            <h1 tw="text-6xl font-bold text-white leading-tight tracking-tight text-left">
-              {title}
-            </h1>
-            <div tw="mt-4 flex items-start">
-              <div tw="h-1 w-24 bg-blue-500 rounded-full" />
-              <div tw="h-1 w-24 bg-purple-500 rounded-full mx-2" />
-              <div tw="h-1 w-24 bg-pink-500 rounded-full" />
-            </div>
-            <p tw="mt-8 text-4xl text-neutral-100 max-w-[900px] font-medium leading-relaxed text-left">
-              {description}
-            </p>
-            <p tw="mt-16 text-2xl text-white text-left">
-              {websiteUrl}
-            </p>
+      <div tw="flex flex-col w-full h-full items-center justify-center" style={{ backgroundColor: '#20132b' }}>
+        <div
+          tw="flex flex-col items-start justify-start max-w-[900px] w-full p-14"
+        >
+          <h1 tw="text-6xl font-bold text-white leading-tight tracking-tight text-left">
+            {title}
+          </h1>
+          <div tw="mt-4 flex items-start">
+            <div tw="h-1 w-24 bg-blue-500/80 rounded-full" />
+            <div tw="h-1 w-24 bg-purple-500/80 rounded-full mx-2" />
+            <div tw="h-1 w-24 bg-pink-500/80 rounded-full" />
           </div>
+          <p tw="mt-8 text-4xl text-neutral-100 max-w-[900px] font-medium leading-relaxed text-left">
+            {description}
+          </p>
+          <p tw="mt-16 text-xl text-neutral-400 text-left">
+            {websiteUrl}
+          </p>
         </div>
       </div>
     ),
